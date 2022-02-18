5cda6be19c1d0a9a2f2abf40665c82b3495804b7
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-18 08:40:19 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { useState, useEffect } from 'react';
-
+import axios from 'axios';
 import './App.scss';
+
+const employeesUrl = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
+const customersUrl = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/customers.json';
 
 function App() {
 	const [employees, setEmployees] = useState([]);
-	// const [customers, setCustomers] = useState([]);
+	const [customers, setCustomers] = useState([]);
 
 	useEffect(() => {
 		(async () => {
-			const url = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
-			const response = await fetch(url);
+			const response = await fetch(employeesUrl);
 			const _employees = await response.json();
 			setEmployees([..._employees]);
+		})();
+		(async () => {
+			const response = await axios.get(customersUrl);
+			setCustomers([...response.data]);
 		})();
 	}, []);
 
@@ -20,9 +26,17 @@ function App() {
 			<h1>Example of fetch and axios</h1>
 			<h2>Employees</h2>
 			<ul>
-				{employees.map((emp, i) => {
+				{employees.map((item, i) => {
 					return (
-						<li key={i}>{emp.firstName} {emp.lastName}</li>
+						<li key={i}>{item.firstName} {item.lastName}</li>
+					)
+				})}
+			</ul>
+			<h2>Customers</h2>
+			<ul>
+				{customers.map((item, i) => {
+					return (
+						<li key={i}>{item.contactName}</li>
 					)
 				})}
 			</ul>
